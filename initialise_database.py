22e045b24aa52a4ad47fbe8488d46202da6cba73
Abhishek Dasgupta <55708520+Abhishek010397@users.noreply.github.com>
2022-11-18 23:18:41 +0530

    Update initialise_database.py

--- initialise_database.py
+++ initialise_database.py
@@ -3,7 +3,7 @@ from Interview.models import User,db
 
 db.create_all()
 user1 = User(username='Admin', password=bcrypt.generate_password_hash(
-    'Cerebrone@28'), role='admin')
+    'xxxx'), role='admin')
 db.session.add(user1)
 db.session.commit()
 
